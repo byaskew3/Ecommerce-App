771762cde596a1b53214fe09306b4855c30a66e4
byaskew3 <byaskew3@gmail.com>
2021-10-15 10:34:09 -0600

    added cart functionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { CallMissedSharp, ShoppingCart } from '@material-ui/icons';
-import { mergeClasses } from '@material-ui/styles';
+import { Link, useLocation } from 'react-router-dom';
 
 import logo from '../../assets/workout.jpg';
 import useStyles from './styles';
 
-const Navbar = () => {
+const Navbar = ({ totalItems }) => {
     const classes = useStyles();
+    const location = useLocation();
+
     
     return (
         <div>
             <AppBar position="fixed" className={CallMissedSharp.appBar} color="inherit">
                 <Toolbar>
-                    <Typography variant="h6" className={classes.title} color="inherit">
+                    <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
                         <img src={logo} alt="workout logo" height="25px" className={classes.image} />
                         ASKEWFIT
                     </Typography>
                     <div className={classes.grow} />
+                    {location.pathname == '/' && (
                     <div className={classes.button}>
-                        <IconButton aria-label="Show cart items" color="inherit">
-                            <Badge badgeContent={2} color="secondary">
+                        <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
+                            <Badge badgeContent={totalItems} color="secondary">
                                 <ShoppingCart />
                             </Badge>
                         </IconButton>
-                    </div>
+                    </div> )}
                 </Toolbar>
             </AppBar>
         </div>
